8b82939fb288549fc42d9f56f2251414b6ce7fbd
Sam Choi <sam.choi@berkeley.edu>
2016-09-06 23:54:32 -0700

    changes

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,3 +1,6 @@
+// js file for scripts
+// Created 9/6/16
+
 function renderLeftBar(url) {
     $.getJSON(url, function(data) {
         $("#scroll-display-left").empty()
@@ -30,8 +33,37 @@ function showStudents() {
             var p = $("<p><a></a></p>");
             $("#scroll-display-left").append(p)
             $("a", p).text(value);
-            $("a", p).attr("href", "/" + value + "/" + "submissions" + "/");
+            // $("a", p).attr("href", "/" + value + "/" + "submissions" + "/");
+            $("a", p).attr("href", "#");
+            $("a", p).click(function() {
+                tester(value);
+            });
         });
+    });
+}
+
+function tester(sid) {
+    $.getJSON("/" + sid + "/" + "submissions" + "/", function(data) {
+        // $("#scroll-display-right").empty()
+        // $.each(data["names"], function(index, value) {
+        //     var p = $("<p><a></a></p>");
+        //     $("#scroll-display-left").append(p)
+        //     $("a", p).text(value);
+        //     // $("a", p).attr("href", "/" + value + "/" + "submissions" + "/");
+        //     $("a", p).attr("href", "#");
+        //     $("a", p).click("showStudents()");
+        // });
+        $("#scroll-display-right").empty();
+        // console.log(JSON.parse(data));
+        // console.log(JSON.stringify(data, undefined, 2));
+        // var stringified = JSON.stringify(data, undefined, 2);
+        // var objectified = $.parseJSON(stringified);
+        // console.log(objectified);
+        // var str = Json.stringify(objectified["Add Up"]["code"], undefined, 2);
+        // var p = $("<p>" + str + "</p>");
+        // $("#scroll-display-right").append(p)
+
+        // $("p", p).text(data["Add Up"]["code"]);
     });
 }
 
